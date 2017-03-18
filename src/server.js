03f824586629a8b85a430a2587c3feb3f2113bc1
Taru Vainikainen <taru.vainikainen@gmail.com>
2017-03-18 18:28:15 +0200

    Fix bot

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ var bot = MessengerPlatform.create({
 }, server);
 app.use(bot.webhook('/'));
 bot.on(MessengerPlatform.Events.MESSAGE, function(userId, message) {
-  bot.sendTextMessage(userId, '<3 <3 <3');
+  bot.sendTextMessage(userId, 'Moi!');
 });
 
 app.get('/', (request, response) => {
